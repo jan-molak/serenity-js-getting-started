08506caff9ede362eebd447472526807ce8ac238
jan-molak <jan.molak@gmail.com>
2016-11-12 17:31:50 +0000

    Updated the protractor imports to work with protractor 4.0.11

--- src/screenplay/ui/todo_list.ts
+++ src/screenplay/ui/todo_list.ts
@@ -1,6 +1,6 @@
 import { Target } from 'serenity-js/lib/screenplay-protractor';
 
-import { by } from 'protractor/globals';
+import { by } from 'protractor';
 
 export class TodoList {
     static What_Needs_To_Be_Done = Target.the('"What needs to be done?" input box')
